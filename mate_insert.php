7519de54ce059d0331dcfa58cdd890549d93bc89
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-22 11:38:12 +0900

    포카 교환글 디비 연동

--- mate_insert.php
+++ mate_insert.php
@@ -7,16 +7,17 @@ $_gender = $_POST['gender'];
 $_idol = $_POST['group'];
 $_own = $_POST['own'];
 $_exch = $_POST['exch'];
-$_deliver = $_GET['delivery'];
+$_deliver = $_POST['delivery'];
 $_state = $_POST['condition'];
+$_file = $_POST['file'];
 $_content = $_POST['content'];
 
-$sql = "insert into card(title, gender, idol, own, exch, deliver, state, content)
-values('$_title', '$_gender', '$_idol', '$_own', '$_exch', '$_deliver', '$_state', '$_content')";
+$sql = "insert into card(title, gender, idol, own, exch, deliver, state, card_img, content)
+values('$_title', '$_gender', '$_idol', '$_own', '$_exch', '$_deliver', '$_state', '$_file', '$_content')";
 
 mysqli_query($conn, $sql);
 ?>
 <script>
-    alert("인상깊은 구절 등록이 완료되었습니다!");
+    alert("포토카드 교환글이 등록되었습니다!");
     location.href="index.html";
 </script>
