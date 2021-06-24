e264e7e3f65d92e193853ef0d83e1a5d09f3cb56
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-24 13:32:32 +0900

    검색

---
+++ search.php
@@ -0,0 +1,8 @@
+<?php
+    $gender = $_POST['gender'];
+    $group = $_POST['group'];
+    $member = $_POST['member'];
+    $member1 = $_POST['member1'];
+
+    echo $gender.$group.$member.$member1;
+?>
