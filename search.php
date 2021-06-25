767b9a5cd14683b6f7d77f79c58c06ec59c41276
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-25 11:17:44 +0900

    Delete

--- search.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $gender = $_POST['gender'];
-    $group = $_POST['group'];
-    $member = $_POST['member'];
-    $member1 = $_POST['member1'];
-
-    echo $gender.$group.$member.$member1;
-?>
